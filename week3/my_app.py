6bc5ad9b5b235c692a5468a41533bec8856d912c
ChimdimmaKosiEgbuonu <cegbuonu@nd.edu>
2026-09-15 09:44:34 -0400

    Update my_app.py

--- week3/my_app.py
+++ week3/my_app.py
@@ -5,4 +5,12 @@ st.title("Hello, streamlit")
 st.write("This is my first Streamlit app.")
 
 if st.button("Click Me!"):
-    st.write
+    st.write("You clicked the button! Nice work!")
+else:
+    st.write("Click the button to see what happens!")
+
+import pandas as pd
+
+st.subheader("Exploring Our Dataset")
+
+df = pd.read_csv("/Users/chimdimmaegbuonu/Documents/Egbuonu-Data-Science-Portfolio/week3/sample_data.csv")
