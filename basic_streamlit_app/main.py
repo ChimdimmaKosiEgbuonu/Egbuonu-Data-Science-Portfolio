336351f9133986a850d5977d54fc2ecaf821d2ad
Chimdimma "Kosi" Egbuonu <cegbuonu@nd.edu>
2026-09-19 16:36:51 -0400

    Update main.py

--- basic_streamlit_app/main.py
+++ basic_streamlit_app/main.py
@@ -1,1 +1,14 @@
+import pandas as pd            
+import seaborn as sns          
+import matplotlib.pyplot as plt  
+import streamlit as st        
 
+
+st.title("Happy Data, Happy Feet: A Streamlit App on the Palmer's Penguins Dataset")
+st.write("This is my first Streamlit app. This app allows you to learn more about penguins through interactive features and engaging visualizations. Thank you for checking it out and have fun!"
+
+
+df = pd.read_csv("penguins.csv")
+
+st.write("**Summary Statistics of the Dataset**")
+st.dataframe(df.describe())
