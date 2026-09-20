8cf09013f19c1346c643976404a7ad174c8bea0f
Chimdimma "Kosi" Egbuonu <cegbuonu@nd.edu>
2026-09-20 15:54:16 -0400

    Update main.py

--- basic_streamlit_app/main.py
+++ basic_streamlit_app/main.py
@@ -9,8 +9,8 @@ st.write("This is my first Streamlit app. This app allows you to learn more abou
 
 
 df = pd.read_csv("basic_streamlit_app/data/penguins.csv")
-st.write("This is the data that this app will be analyzing")
+st.write("This is the data that this app will be analyzing:")
 st.dataframe(df)
 
-st.write("**Summary Statistics of the Dataset**")
+st.write("**Summary Statistics of the Dataset:**")
 st.dataframe(df.describe())
